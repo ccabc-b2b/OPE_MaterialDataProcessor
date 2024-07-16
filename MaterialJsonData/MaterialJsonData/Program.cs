97ea1303d451fe49b9841bb5ff233f73e651a2b2
AvishekThakur23 <avishek.thakur@infosys.com>
2024-07-16 11:11:53 +0400

    Added .Net Standard code

--- MaterialJsonData/MaterialJsonData/Program.cs
+++ MaterialJsonData/MaterialJsonData/Program.cs
@@ -1,28 +1,29 @@
 ﻿using Azure.Identity;
 using Microsoft.Extensions.Configuration;
+using System;
 
 namespace MaterialJsonData
-{
-    class Program
     {
-        static void Main(string[] args)
+    class Program
         {
-            try
+        static void Main(string[] args)
             {
+            try
+                {
                 ConfigurationBuilder builder = new ConfigurationBuilder();
                 builder.AddAzureKeyVault(new Uri(Properties.Settings.Default.KeyVaultURI), new DefaultAzureCredential());
                 IConfiguration configuration = builder.Build();
                 MaterialJsonData materialData = new MaterialJsonData(configuration);
                 materialData.LoadMaterialData();
-            }
+                }
             catch (Exception ex)
-            {
+                {
                 ConfigurationBuilder builder = new ConfigurationBuilder();
                 builder.AddAzureKeyVault(new Uri(Properties.Settings.Default.KeyVaultURI), new DefaultAzureCredential());
                 IConfiguration configuration = builder.Build();
                 Logger logger = new Logger(configuration);
                 logger.ErrorLogData(ex, ex.Message);
+                }
             }
         }
     }
-}
